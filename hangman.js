523ae6800d303064417e19665b4d2d01ab0bdb22
AaronZ314 <aaronminxz@gmail.com>
2025-10-20 10:54:08 -0400

    Added guess letter functionality

--- hangman.js
+++ hangman.js
@@ -1,15 +1,35 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
 
 var word = "";
+var guesses = ""
 
 function newGame(){
     var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
-    var word = POSSIBLE_WORDS[randomIndex];
+    word = POSSIBLE_WORDS[randomIndex];
+    guesses = "";
+    updatePage();
+}
 
+function guessLetter(){
+    var input = document.getElementById("guess");
+    var letter = input.value;
+    guesses += letter;
+    updatePage();
+}
+
+function updatePage(){
     var clueString = "";
-    for(var i = 0;i<word.length;i++) {
-        clueString+="_ ";
+    for(var i = 0; i<word.length; i++) {
+        var currentLetter = word.charAt(i);
+        if(guesses.indexOf(currentLetter) >= 0){
+            clueString+=currentLetter+ " ";
+        }
+        else
+            clueString+="_ ";
     }
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
+
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guessed Letters: " + guesses;
 }
