919180cde7e16eb4472ab31e171d72acedc910d2
AaronZ314 <aaronminxz@gmail.com>
2025-11-02 23:17:17 -0500

    Added lose condition

--- hangman.js
+++ hangman.js
@@ -1,20 +1,31 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
-
+const MAX_GUESSES = 6;
 var word = "";
-var guesses = ""
+var guesses = "";
+var guess_count = MAX_GUESSES;
+
 
 function newGame(){
+    inputArea.hidden = false;
     var randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses = "";
+    guess_count = MAX_GUESSES;
     updatePage();
 }
 
 function guessLetter(){
     var input = document.getElementById("guess");
     var letter = input.value;
+    if(word.indexOf(letter) < 0) {
+        guess_count--;
+        if(guess_count <= 0){
+            inputArea.hidden = true;
+        }
+    }
     guesses += letter;
     updatePage();
+    input.value = "";
 }
 
 function updatePage(){
@@ -27,9 +38,20 @@ function updatePage(){
         else
             clueString+="_ ";
     }
+    //update the clue string
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
 
+    //update the guesses
     var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed Letters: " + guesses;
+    if (guess_count <= 0) {
+        guessArea.innerHTML = "You lose!";
+    }
+
+    //update the image
+    var image = document.getElementById("hangmanImage");
+    image.src = "images/hangman"+guess_count+".gif";
+
+    //TODO: CREATE WIN CONDITION. DO NOT LET PEOPLE GUESS THE SAME LETTER TWICE (IGNORE THE SECOND GUESS IF IT HAPPENS)
 }
